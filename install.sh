452f2de3ea2983dcbf4751dda5da948b0a3e6970
Amal Khailtash <amal.khailtash@gmail.com>
2013-10-19 18:06:03 -0400

    Update to match my .vim

--- install.sh
+++ install.sh
@@ -11,18 +11,23 @@ die() {
 
 cd ~
 
+echo "Making backup of existing ~/.vim ~/.vimrc ~/.gvimrc ..."
+
 [ -e ~/.vim ] && mv -i ~/.vim ~/.vim.backup
 [ -e ~/.vimrc ] && mv -i ~/.vimrc ~/.vimrc.backup
 [ -e ~/.gvimrc ] && mv -i ~/.gvimrc ~/.gvimrc.backup
 
 git clone git://github.com/amal-khailtash/dotvim.git .vim
 
+# Set up the symlinks to vimrc and gvimrc
+echo "Making symlinks ~/.vimrc ~/.gvimrc ..."
+ln -s ~/.vim/_vimrc ~/.vimrc
+ln -s ~/.vim/_gvimrc ~/.gvimrc
+
 cd ~/.vim
 
-# Set up the symlinks to vimrc and gvimrc
-ln -s ~/.vim/vimrc ~/.vimrc
-ln -s ~/.vim/gvimrc ~/.gvimrc
-
+echo "Initializing Vundle"
 git submodule init
 git submodule update
 
+echo "All Done"
